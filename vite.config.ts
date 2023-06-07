cde5452ea99e75da8f49fa74d11b2d61ad3833d8
Kazumi Inada <hello@nandenjin.com>
2023-06-07 23:02:00 +0900

    chore: Set build target as browser lists

--- vite.config.ts
+++ vite.config.ts
@@ -20,5 +20,8 @@ const manifest = defineManifest({
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: ['chrome89', 'edge89', 'firefox89'],
+  },
   plugins: [tsConfigPaths(), react(), crx({ manifest })],
 })
