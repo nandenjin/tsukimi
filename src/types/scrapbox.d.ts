ad77ed4308796ed429c6c4cb78147c6f5b70ac2c
Kazumi Inada <hello@nandenjin.com>
2024-10-21 16:21:38 +0900

    refactor: Create `src/popup` directory

--- src/types/scrapbox.d.ts
+++ src/types/scrapbox.d.ts
@@ -1,10 +1,16 @@
-// ref: https://github.com/scrapbox-jp/types
+/** @see https://github.com/scrapbox-jp/types */
 
+/**
+ * Event emitter interface. (local definition in the project)
+ */
 interface EventEmitter {
   on(event: string, listener: (...args: unknown[]) => void): this
   once(event: string, listener: (...args: unknown[]) => void): this
 }
 
+/**
+ * A line in a page. Exposed at `scrapbox.Page.lines`.
+ */
 export type Line = {
   id: string
   text: string
@@ -18,12 +24,22 @@ export type Line = {
   }
 }
 
+/**
+ * Layout ids of page.
+ *
+ * @see https://github.com/scrapbox-jp/types/blob/main/layout.ts
+ */
+export type Layout = 'page' | 'project' | 'stream'
+
 export type Page<T extends Layout> = {
   get lines(): T extends 'page' ? Line[] : null
   get title(): T extends 'page' ? string : null
   get id(): T extends 'page' ? string : null
 }
 
+/**
+ * Scrapbox project
+ */
 export type Project = {
   get pages(): (Page<'page'> & {
     exists: boolean
@@ -34,7 +50,14 @@ export type Project = {
   })[]
 }
 
+/**
+ * Type of `window.scrapbox`.
+ */
 export type Scrapbox = EventEmitter & {
-  Page: Page
+  Page: Page<Layout>
   Project: Project
+}
+
+declare global {
+  const scrapbox: readonly Scrapbox
 }
